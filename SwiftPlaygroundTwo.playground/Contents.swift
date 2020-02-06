f67a83464623baa21d470a17e445caf4c459f079
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-02-06 11:18:39 -1000

    Completed Problem Sets

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var str = "Hello, playground"
 
-
+// If/Else Statements
 var goodGrade = "I've been doing all my work"
 if goodGrade == "I've been doing all my work"
 {
@@ -13,8 +13,8 @@ else
     print("I need to work harder in this class")
 }
 
+// Using Boolean variables in if Statements
 var gradeA = true
-    
 if gradeA
 {
     print("I really like this class")
@@ -22,4 +22,37 @@ if gradeA
 else
 {
     print("I'm not so sure about coding")
+}
+
+// On Your Own - Problem Set 1 - integer variables
+var Manoa = 5
+var WestOahu = 8
+var comparison = Manoa > WestOahu
+if Manoa > WestOahu
+{
+    print("GO BOWZ")
+}
+else
+{
+    print("WESTSIIIDE")
+}
+
+// On Your Own - Problem Set 2 - string variables
+var UHWO = "Go Pueos! "
+var UniversityofHawaiiWestOahu = "Go Pueos! "
+if UHWO == UniversityofHawaiiWestOahu
+{
+    print(UHWO + UniversityofHawaiiWestOahu)
+}
+
+// On Your Own - Problem Set 3 - boolean variable
+var CM161 = false
+var ENG200 = true
+if CM161 == true
+{
+    print("I would rather write an essay")
+}
+else
+{
+    print("I would rather code")
 }
